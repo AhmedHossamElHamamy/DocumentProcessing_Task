bf8b78430a32df8ea4868886c569faa3f7836a3a
AhmedHossamElHamamy <ahmed110hossam@gmail.com>
2025-01-04 20:11:15 +0200

    Image and PDF Models and Migrations

--- Document/models.py
+++ Document/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Image(models.Model):
+    file = models.ImageField(upload_to='images/')  # Saves to 'media/images/'
+    width = models.IntegerField(blank=True, null=True)
+    height = models.IntegerField(blank=True, null=True)
+    channels = models.IntegerField(blank=True, null=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.file.name
+
+class PDF(models.Model):
+    file = models.FileField(upload_to='pdfs/')  # Saves to 'media/pdfs/'
+    num_pages = models.IntegerField(blank=True, null=True)
+    page_width = models.FloatField(blank=True, null=True)
+    page_height = models.FloatField(blank=True, null=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.file.name
