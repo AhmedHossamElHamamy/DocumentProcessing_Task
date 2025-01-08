2c3ccb6fc61ffad18a3ab02cfa55bdbbf923cbc1
AhmedHossamElHamamy <ahmed110hossam@gmail.com>
2025-01-08 17:42:48 +0200

    [URLS]:Map root URL to my app's URLs

--- Document_Processing/urls.py
+++ Document_Processing/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('Document.urls')),  # Include the app's URLs
+    path('', include('Document.urls')),  # Map root URL to your app's URLs
 
 ]
 if settings.DEBUG:
